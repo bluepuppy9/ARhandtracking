b4f0b30e34938d8f9f45dbb72d15c2a30b773ffa
Benjamin Bernadskiy <78938589+bluepuppy9@users.noreply.github.com>
2025-07-15 11:50:41 -0400

    WAAZXAHHHHHHHHHHHHHHHHHH

--- vuear/vite.config.ts
+++ vuear/vite.config.ts
@@ -3,6 +3,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import fs from 'fs'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,4 +17,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'cert/key.pem') ),
+      cert: fs.readFileSync(path.resolve(__dirname, 'cert/cert.pem'))
+    },
+    port: 3000,
+    host: true,
+  },
+
+
 })
