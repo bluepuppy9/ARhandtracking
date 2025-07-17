632ed26ec639e2ac8c5eb1352d55e11a8ec477b1
Benjamin Bernadskiy <78938589+bluepuppy9@users.noreply.github.com>
2025-07-17 10:27:48 -0400

    MUTLI RATS
    instead of .mind

--- vueAR/src/components/mindarViewer.vue
+++ vueAR/src/components/mindarViewer.vue
@@ -1,7 +1,7 @@
 <template>
     <a-scene
       ref="sceneRef"
-      mindar-image="imageTargetSrc: /targetsRAT.mind; autoStart: false; uiLoading: no; uiError: no; uiScanning: no;" 
+      mindar-image="imageTargetSrc: /targetsSPONGE.mind; maxTrack: 2; autoStart: false; uiLoading: no; uiError: no; uiScanning: no;"
       color-space="sRGB"
       embedded
       renderer="colorManagement: true, physicallyCorrectLights"
@@ -9,20 +9,20 @@
       device-orientation-permission-ui="enabled: false"
     >
       <a-assets>
-  
+ 
         <a-asset-item
           id="avatarModel"
           src="/rat.glb"
           crossorigin
               ></a-asset-item>
       </a-assets>
-  
+ 
       <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
-  
+ 
       <a-entity
         v-for="i in 2"
         :key="i"
-        :mindar-image-target="0"
+        :mindar-image-target="'targetIndex: ' + (i - 1)"
       >
         <a-gltf-model
           rotation="0 0 0"
@@ -32,13 +32,13 @@
           animation="property: rotation; to: 0 360 0; dur: 1000; easing: linear; loop: true"
         ></a-gltf-model>
       </a-entity>
-      
+     
     </a-scene>
 </template>
-  
+ 
 <script setup>  
     import { ref } from 'vue'
-  
+ 
     const sceneRef = ref(null)
     defineExpose({
       sceneRef
