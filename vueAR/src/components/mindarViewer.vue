15ce03ccea4c39d4df7ec011bc3d47f2f17bbb33
pichu1298 <kristinachuchu@gmail.com>
2025-07-17 11:06:04 -0400

    stff

--- vueAR/src/components/mindarViewer.vue
+++ vueAR/src/components/mindarViewer.vue
@@ -1,8 +1,14 @@
-<script setup>  
-  import { ref, defineExpose } from 'vue'
+<script setup>
+import { ref, defineExpose } from 'vue'
 
-  const sceneRef = ref(null)
-  defineExpose({ sceneRef })
+const sceneRef = ref(null)
+defineExpose({ sceneRef })
+
+function handleModelClick(event) {
+  console.log('Model clicked!'); // Log to confirm the click event
+  const clickedEl = event.target; // Get the clicked element
+  clickedEl.setAttribute('scale', '0.05 0.05 0.05'); // Shrink the model
+}
 </script>
 
 <template>
@@ -16,25 +22,31 @@
     device-orientation-permission-ui="enabled: false"
   >
     <a-assets>
-
       <a-asset-item
         id="avatarModel"
         src="/rat.glb"
         crossorigin
-            ></a-asset-item>
+      ></a-asset-item>
     </a-assets>
 
-    <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
+    <a-camera 
+      position="0 0 0"
+      cursor="rayOrigin: mouse;"
+      raycaster="objects: .clickable" 
+      look-controls="enabled: false">
+    </a-camera>
 
     <a-entity mindar-image-target="targetIndex: 0">
-      
       <a-gltf-model
-        rotation="0 0 0 "
+        class="clickable"
+        @click="handleModelClick"
+        ref="ratModel"
+        rotation="0 0 0"
         position="0 0 0.1"
-        scale="0.1 0.1 0.1"
+        scale="1 1 1"
         src="#avatarModel"
         animation="property: rotation; to: 0 360 0; dur: 1000; easing: linear; loop: true;"
-        ></a-gltf-model>
+      ></a-gltf-model>
     </a-entity>
   </a-scene>
 </template>
