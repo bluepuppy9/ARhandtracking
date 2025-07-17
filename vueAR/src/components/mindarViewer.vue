444d25fe2efc53fdcedb805f066dbb5383f44596
pichu1298 <kristinachuchu@gmail.com>
2025-07-17 11:51:45 -0400

    c

--- vueAR/src/components/mindarViewer.vue
+++ vueAR/src/components/mindarViewer.vue
@@ -4,11 +4,27 @@ import { ref, defineExpose } from 'vue'
 const sceneRef = ref(null)
 defineExpose({ sceneRef })
 
-function handleModelClick(event) {
-  console.log('Model clicked!'); // Log to confirm the click event
-  const clickedEl = event.target; // Get the clicked element
-  clickedEl.setAttribute('scale', '0.05 0.05 0.05'); // Shrink the model
-}
+// function handleModelClick() {
+//   console.log('Model clicked!'); // Log to confirm the click event
+//   const clickedEl = ratModel.value;
+//   clickedEl.setAttribute('scale', '0.1 0.1 0.1'); // Shrink the model
+// }
+
+AFRAME.registerComponent('change-color-on-hover', {
+    schema: {
+      color: {default: 'red'}
+    },
+
+    init: function () {
+      var data = this.data;
+      var el = this.el;  // <a-box>
+
+      el.addEventListener('click', function () {
+        el.setAttribute('color', data.color);
+      });
+    }
+  });
+
 </script>
 
 <template>
@@ -29,24 +45,35 @@ function handleModelClick(event) {
       ></a-asset-item>
     </a-assets>
 
-    <a-camera 
-      position="0 0 0"
-      cursor="rayOrigin: mouse;"
-      raycaster="objects: .clickable" 
-      look-controls="enabled: false">
-    </a-camera>
+<a-camera 
+  position="0 0 0"
+  cursor="rayOrigin: mouse; fuse: false;"
+  raycaster="objects: .clickable; interval: 50; near: 0.1; far: 100;"
+  look-controls="enabled: false">
+</a-camera>
 
     <a-entity mindar-image-target="targetIndex: 0">
-      <a-gltf-model
-        class="clickable"
+      
+      <!-- <a-gltf-model
         @click="handleModelClick"
+        @mousedown="handleModelClick"
+        class="clickable"
         ref="ratModel"
         rotation="0 0 0"
-        position="0 0 0.1"
+        position="0 0 -1"
         scale="1 1 1"
         src="#avatarModel"
         animation="property: rotation; to: 0 360 0; dur: 1000; easing: linear; loop: true;"
-      ></a-gltf-model>
+      >
+    </a-gltf-model> -->
+    <a-box
+        position="0 0 -2"
+        scale=".5 .5 .5"
+        opacity="1"
+        class="clickable"
+        color="red"
+        change-color-on-hover="color: blue"
+      ></a-box>
     </a-entity>
   </a-scene>
 </template>
