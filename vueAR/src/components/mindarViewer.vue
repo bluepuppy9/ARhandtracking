ae17a2c0a1ddc01240ace390fe61c3e85cecd715
Benjamin Bernadskiy <78938589+bluepuppy9@users.noreply.github.com>
2025-07-17 23:59:40 -0400

    make rat more stable

--- vueAR/src/components/mindarViewer.vue
+++ vueAR/src/components/mindarViewer.vue
@@ -1,10 +1,10 @@
 <template>
   <a-scene
     ref="sceneRef"
-    mindar-image="imageTargetSrc: /targetsQR2.mind; autoStart: false; uiLoading: no; uiError: no; uiScanning: no;"
+    mindar-image="imageTargetSrc: /targetsXS.mind; autoStart: false; uiLoading: no; uiError: no; uiScanning: no; filterMinCF:0.0001; filterBeta: 0.001;"
     color-space="sRGB"
     embedded
-    renderer="colorManagement: true, physicallyCorrectLights"
+    renderer="colorManagement: true, physicallyCorrectLights : true; precision: high;"
     vr-mode-ui="enabled: false"
     device-orientation-permission-ui="enabled: false"
   >
@@ -12,11 +12,17 @@
       <a-asset-item id="avatarModel" src="/rat.glb" crossorigin></a-asset-item>
     </a-assets>
 
-    <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
+    <a-camera
+      mindar-camera
+      video="width: 1920; height: 1080;"
+      position="0 0 0"
+      look-controls="enabled: false"
+    ></a-camera>
 
     <a-entity mindar-image-target="targetIndex: 0">
       <a-gltf-model rotation="90 0 0" position="0 0 0.1" scale="0.5 0.5 0.5" src="#avatarModel">
       </a-gltf-model>
+      <a-sphere radius="0.01" color="red" scale="20 20 20"></a-sphere>
     </a-entity>
   </a-scene>
 </template>
