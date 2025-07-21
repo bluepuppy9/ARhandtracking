47244d55c8759b88c428a3f152f56e98178c2c22
Benjamin Bernadskiy <78938589+bluepuppy9@users.noreply.github.com>
2025-07-21 12:45:08 -0400

    4 qr codes testing

--- vueAR/src/components/mindarViewer.vue
+++ vueAR/src/components/mindarViewer.vue
@@ -1,7 +1,7 @@
 <template>
   <a-scene
     ref="sceneRef"
-    mindar-image="imageTargetSrc: /targetsTEST.mind; maxTrack: 2; autoStart: false; uiLoading: no; uiError: no; uiScanning: no;"
+    mindar-image="imageTargetSrc: /targets (33).mind; maxTrack: 4; autoStart: false; uiLoading: no; uiError: no; uiScanning: no;"
     color-space="sRGB"
     embedded
     renderer="colorManagement: true, physicallyCorrectLights"
@@ -14,14 +14,9 @@
 
     <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
 
-    <a-entity v-for="i in 2" :key="i" :mindar-image-target="'targetIndex: ' + (i - 1)">
-      <a-gltf-model
-        rotation="0 0 0"
-        position="0 0 0.1"
-        scale="0.1 0.1 0.1"
-        src="#avatarModel"
-        animation="property: rotation; to: 0 360 0; dur: 1000; easing: linear; loop: true"
-      ></a-gltf-model>
+    <a-entity v-for="i in 4" :key="i" :mindar-image-target="'targetIndex: ' + (i - 1)">
+      <a-gltf-model rotation="90 0 0" position="0 0 0.1" scale="0.5 0.5 0.5" src="#avatarModel">
+      </a-gltf-model>
     </a-entity>
   </a-scene>
 </template>
