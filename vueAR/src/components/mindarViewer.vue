c69739c56c5c03d6175e466334a179fa5d381b8e
pichu1298 <kristinachuchu@gmail.com>
2025-07-22 09:59:54 -0400

    stuff

--- vueAR/src/components/mindarViewer.vue
+++ vueAR/src/components/mindarViewer.vue
@@ -15,15 +15,34 @@
     <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
 
     <a-entity v-for="i in 4" :key="i" :mindar-image-target="'targetIndex: ' + (i - 1)">
-      <a-gltf-model rotation="90 0 0" position="0 0 0.1" scale="0.5 0.5 0.5" src="#avatarModel">
+      <a-gltf-model rotation="90 0 0" position="0 0 0.1" scale="0.5 0.5 0.5" src="#avatarModel" class="clickable" ref="ratModel">
       </a-gltf-model>
     </a-entity>
   </a-scene>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
+import TimingGame from './TimingGame.vue';
+//Functions to hopefully start the minigame
+const ratModel = ref(null)
 
+onMounted(() => {
+  window.addEventListener("DOMContentLoaded", () => {
+    const rat = ratModel.value
+    if (rat) {
+      rat.addEventListener("click", () =>{
+        console.log("Rat clicked!")
+
+        //start minigame in here or spawn it after the rat is clicked
+
+      })
+    }
+  })
+})
+
+
+//scene stuff
 const sceneRef = ref(null)
 defineExpose({
   sceneRef,
---
+++ vuear/src/components/TimingGame.vue
@@ -0,0 +1,12 @@
+<template>
+</template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  
+  const sceneRef = ref(null)
+  defineExpose({
+    sceneRef,
+  })
+  </script>
+  
