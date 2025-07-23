6fff77d15af609141def38ea1b1be913b2e503bb
Benjamin Bernadskiy <78938589+bluepuppy9@users.noreply.github.com>
2025-07-23 08:54:43 -0400

    homeview

--- vueAR/src/components/mindarViewer.vue
+++ vueAR/src/components/mindarViewer.vue
@@ -14,7 +14,7 @@
 
     <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
 
-    <a-entity v-for="i in 4" :key="i" :mindar-image-target="'targetIndex: ' + (i - 1)">
+    <a-entity v-for="i in 3" :key="i" :mindar-image-target="'targetIndex: ' + (i - 1)">
       <a-gltf-model rotation="90 0 0" position="0 0 0.1" scale="0.5 0.5 0.5" src="#avatarModel">
       </a-gltf-model>
     </a-entity>
