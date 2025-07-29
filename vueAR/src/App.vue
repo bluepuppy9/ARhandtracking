6abb89f09b202d1679c8fcd6571154d068a763a1
Benjamin Bernadskiy <78938589+bluepuppy9@users.noreply.github.com>
2025-07-29 12:03:01 -0400

    im the greek god of rats(it works now homepage)

--- vueAR/src/App.vue
+++ vueAR/src/App.vue
@@ -1,46 +1,9 @@
 <template>
   <div class="app">
-    <div>
-      <button @click="handleARSystems()">{{ title }}</button>
-    </div>
-
-    <div class="container">
-      <mindar-viewer ref="mindarViewerRef" />
-    </div>
+    <RouterView />
   </div>
 </template>
 
 <script setup>
-import './libs/mindar/mindar-image.prod.js'
-import 'aframe'
-import './libs/mindar/mindar-image-aframe.prod.js'
-import MindarViewer from './components/mindarViewer.vue'
-import { ref } from 'vue'
-
-const mindarViewerRef = ref(null)
-const title = ref('START')
-
-function handleARSystems() {
-  const sceneEl = mindarViewerRef.value.sceneRef
-  const arSystem = sceneEl.systems['mindar-image-system']
-  if (title.value == 'START') {
-    arSystem.start()
-    title.value = 'STOP'
-  } else {
-    arSystem.stop()
-    title.value = 'START'
-  }
-}
+import { RouterView } from 'vue-router'
 </script>
-
-<style scoped>
-.app {
-  text-align: center;
-}
-
-.container {
-  height: 100vh;
-  width: 100vw;
-  overflow: hidden;
-}
-</style>
---
+++ vuear/src/views/HomeView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="main">
+    <login v-if="!isLoggedIn" />
+    <button @click="handleARSystems()">{{ title }}</button>
+
+    <div class="container">
+      <mindar-viewer ref="mindarViewerRef" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed, ref } from 'vue'
+import login from '../components/login.vue'
+import '../libs/mindar/mindar-image.prod.js'
+import 'aframe'
+import '../libs/mindar/mindar-image-aframe.prod.js'
+import MindarViewer from '../components/mindarViewer.vue'
+import { useUserStore } from '../stores/userStore'
+const mindarViewerRef = ref(null)
+const title = ref('START')
+const userStore = useUserStore()
+
+const isLoggedIn = computed(() => userStore.isLoggedIn)
+
+function handleARSystems() {
+  const sceneEl = mindarViewerRef.value.sceneRef
+  const arSystem = sceneEl.systems['mindar-image-system']
+  if (title.value == 'START') {
+    arSystem.start()
+    title.value = 'STOP'
+  } else {
+    arSystem.stop()
+    title.value = 'START'
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+}
+</style>
