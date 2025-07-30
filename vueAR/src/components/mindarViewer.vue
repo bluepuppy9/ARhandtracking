0e465f474bf0748ba15cbc1aaee778b585c993cf
pichu1298 <kristinachuchu@gmail.com>
2025-07-30 13:00:36 -0400

    checking

--- vueAR/src/components/mindarViewer.vue
+++ vueAR/src/components/mindarViewer.vue
@@ -1,7 +1,7 @@
 <template>
   <a-scene
     ref="sceneRef"
-    mindar-image="imageTargetSrc: /targets (38).mind; maxTrack: 4; autoStart: false; uiLoading: no; uiError: no; uiScanning: no;"
+    mindar-image="imageTargetSrc: /ratstop.mind; maxTrack: 4; autoStart: false; uiLoading: no; uiError: no; uiScanning: no;"
     color-space="sRGB"
     embedded
     renderer="colorManagement: true, physicallyCorrectLights"
@@ -17,7 +17,7 @@
     </a-assets>
     <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
 
-    <a-entity v-for="i in 2" :key="i" :mindar-image-target="'targetIndex: ' + (i - 1)">
+    <a-entity v-for="i in 3" :key="i" :mindar-image-target="'targetIndex: ' + (i - 1)" ref="targets">
       <a-gltf-model rotation="90 0 0" position="0 0 0.1" scale="0.5 0.5 0.5" src="#ratModel">
       </a-gltf-model>
     </a-entity>
@@ -29,12 +29,21 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref, useTemplateRef } from 'vue'
+
+const targets = useTemplateRef('targets')
 
 const sceneRef = ref(null)
 defineExpose({
   sceneRef,
 })
+
+onMounted(()=>{
+  targets.value[3].addEventListener('targetFound', ()=> {
+    //spawn items here
+  })
+})
+
 
 const UserRats = ref({
   0: { type: 'normal', scale: 2, caught: false },
