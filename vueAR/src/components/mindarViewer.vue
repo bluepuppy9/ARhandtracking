6676e7dfb13f082495a0642e8d47009bb912a585
pichu1298 <62480135+pichu1298@users.noreply.github.com>
2025-08-07 10:56:56 -0400

    4 minigame one (#13)
    Co-authored-by: Benjamin Bernadskiy <78938589+bluepuppy9@users.noreply.github.com>

--- vueAR/src/components/mindarViewer.vue
+++ vueAR/src/components/mindarViewer.vue
@@ -1,4 +1,5 @@
 <template>
+    <TimingMinigame v-if="ratFound" />
   <a-scene
     ref="sceneRef"
     mindar-image="imageTargetSrc: /targets (41).mind; maxTrack: 4; autoStart: false; uiLoading: no; uiError: no; uiScanning: no;"
@@ -9,20 +10,22 @@
     device-orientation-permission-ui="enabled: false"
   >
     <a-assets>
-      <a-asset-item id="ratModel" src="/rat.glb" crossorigin></a-asset-item>
+      <a-asset-item id="avatar-model" src="/rat.glb" crossorigin></a-asset-item>
     </a-assets>
 
     <a-assets>
-      <a-asset-item id="shinyModel" src="/shinyRat.glb" crossorigin></a-asset-item>
+      <a-asset-item id="shiny-model" src="/shinyRat.glb" crossorigin></a-asset-item>
     </a-assets>
+
     <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
 
-    <a-entity v-for="(rat, i) in UserRats" :key="i" :mindar-image-target="'targetIndex: ' + i">
+    <a-entity v-for="(rat, i) in UserRats" :key="i" :mindar-image-target="'targetIndex: ' + i" ref="targets">
       <a-gltf-model
+        ref="ratModels"
         rotation="90 0 0"
         position="0 0 0.1"
         scale="0.1 0.1 0.1"
-        :src="'#' + rat.type + 'Model'"
+        :src="'#' + rat.type + '-model'"
       >
       </a-gltf-model>
     </a-entity>
@@ -30,9 +33,35 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { onMounted, onUnmounted, ref, useTemplateRef } from 'vue'
+import TimingMinigame from './TimingMinigame.vue'
 
-const sceneRef = ref(null)
+const ratFound = ref(false)
+const targets = useTemplateRef('targets')
+
+function findingRat(){
+  ratFound.value = true
+}
+function lostRat(){
+  ratFound.value = false
+}
+
+onMounted(() => {
+  const targetElements = targets.value;
+  targetElements.forEach((target) => {
+    target.addEventListener("targetFound", findingRat)
+    target.addEventListener("targetLost", lostRat)
+  })
+})
+
+onUnmounted(() =>{
+  const targets = targets.value;
+  targets.forEach((target) => {
+    target.removeEventListener("targetFound", findingRat)
+    target.removeEventListener("targetLost", lostRat)
+  })
+})
+const sceneRef = useTemplateRef('sceneRef')
 defineExpose({
   sceneRef,
 })
@@ -42,3 +71,4 @@ const UserRats = ref([
   { type: 'shiny', scale: 2, caught: false, id: 1 },
 ])
 </script>
+
