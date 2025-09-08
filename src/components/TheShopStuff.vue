19b9be459565993b6c4f2baf68ca1a737e1a41d0
Kristina Chu <kristinachuchu@gmail.com>
2025-09-08 17:23:40 -0400

    +const not let

--- src/components/TheShopStuff.vue
+++ src/components/TheShopStuff.vue
@@ -24,11 +24,10 @@ const timer = ref(0)
 function startTimer() {
   obtainedAlready.value = true
   timer.value = 10
-  let countDown = setInterval(() => {
+  const countDown = setInterval(() => {
     if (timer.value > 1) {
       timer.value -= 1
-    }
-    else if (timer.value === 1) {
+    } else if (timer.value === 1) {
       obtainedAlready.value = false
       clearInterval(countDown)
     }
